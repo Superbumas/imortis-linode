ecdbbda992520dbe7a297269f0a7314c300d4a92
Andrius <wimass@gmail.com>
2024-09-11 00:11:30 +0100

    s

--- forms.py
+++ forms.py
@@ -1,5 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField
+from wtforms import StringField, TextAreaField, DateField, FileField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+class ProfileForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=100)])
+    bio = TextAreaField('Bio', validators=[DataRequired()])
+    date_of_birth = DateField('Date of Birth', format='%Y-%m-%d', validators=[DataRequired()])
+    date_of_death = DateField('Date of Death', format='%Y-%m-%d', validators=[DataRequired()])
+    profile_picture = FileField('Profile Picture')
+    submit = SubmitField('Create Profile')
 
 class DeleteProfileForm(FlaskForm):
     submit = SubmitField('Delete')
