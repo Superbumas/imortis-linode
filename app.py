dbd1cc352a62dfbedfb886cfa0d76103b650c90a
Andrius <wimass@gmail.com>
2024-09-11 09:43:24 +0100

    s

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ class Profile(db.Model):
     city = db.Column(db.String(50), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def __repr__(self):
         return f'<Profile {self.name}>'
