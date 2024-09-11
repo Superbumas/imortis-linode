3271fadcb1e3b7e86752c8ac6b876b2432b92015
Andrius <wimass@gmail.com>
2024-09-11 09:29:29 +0100

    s

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, DateField, FileField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Optional
 from wtforms.fields import SelectField
 
 # List of countries
