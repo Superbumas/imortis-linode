1ed16e57ce6c29a0fb7b571728cb56e2d5b1e944
Andrius <wimass@gmail.com>
2024-09-11 12:18:07 +0100

    s

--- app.py
+++ app.py
@@ -57,18 +57,10 @@ class Profile(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    
     def __repr__(self):
         return f'<Profile {self.name}>'
 
 
-
-
-class Timeline(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, nullable=False)
-    event = db.Column(db.String(255), nullable=False)
-    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
 
 # Forms
 class RegistrationForm(FlaskForm):
