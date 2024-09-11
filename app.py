4ef041ab77a04a41abba7d3318ac91ebe436f07a
Andrius <wimass@gmail.com>
2024-09-11 13:17:44 +0100

    s

--- app.py
+++ app.py
@@ -137,12 +137,13 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and check_password_hash(user.password, form.password.data):
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
             login_user(user)
-            return redirect(url_for('dashboard'))  # Redirect to dashboard after login
+            flash('Logged in successfully.', 'success')
+            return redirect(url_for('dashboard'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Invalid email or password.', 'danger')
     return render_template('login.html', form=form)
 
 @app.route('/logout')
