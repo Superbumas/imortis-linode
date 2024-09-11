e84204427371a8f8f1fbb2bd347c25f8a34017f4
Andrius <wimass@gmail.com>
2024-09-11 09:37:39 +0100

    s

--- app.py
+++ app.py
@@ -86,14 +86,6 @@ class TimelineForm(FlaskForm):
     date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
     event = StringField('Event', validators=[DataRequired(), Length(max=255)])
 
-class ProfileForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), Length(min=2, max=100)])
-    bio = TextAreaField('Bio', validators=[DataRequired()])
-    date_of_birth = DateField('Date of Birth', format='%Y-%m-%d', validators=[DataRequired()])
-    date_of_death = DateField('Date of Death', format='%Y-%m-%d', validators=[DataRequired()])
-    profile_picture = FileField('Profile Picture')
-    submit = SubmitField('Create Profile')
-
 class SettingsForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     current_password = PasswordField('Current Password', validators=[DataRequired()])
