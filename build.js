4a2c67e8195f66c3f26a0f68378be18c53bcfca7
Andrius <wimass@gmail.com>
2024-09-14 13:38:51 +0100

    s

--- build.js
+++ build.js
@@ -1,14 +1,15 @@
 const { readFileSync, writeFileSync } = require('fs');
-const { compileTemplate, compileScript } = require('@vue/compiler-sfc');
+const { compileTemplate, compileScript, parse } = require('@vue/compiler-sfc');
 
 const filePath = 'static/components/ProfileTimeline.vue';
 const fileContent = readFileSync(filePath, 'utf-8');
 
-const { descriptor } = require('@vue/compiler-sfc').parse(fileContent);
+const { descriptor } = parse(fileContent);
 
 const { code: templateCode } = compileTemplate({
   source: descriptor.template.content,
   filename: filePath,
+  id: 'ProfileTimeline'  // Add the id option here
 });
 
 const { code: scriptCode } = compileScript(descriptor, {
