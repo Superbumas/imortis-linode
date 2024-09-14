3fe22bbb0ece53b6ab82bf75372db6d1fc2ffc42
Andrius <wimass@gmail.com>
2024-09-14 12:15:33 +0100

    s

--- create_db.py
+++ create_db.py
@@ -1,4 +1,5 @@
-from app import db, app
+from app import db, app, Profile, User, TimelineEvent, ProfileForm, TimelineEventForm, CreateProfileForm, EditProfileForm, EditTimelineForm, DeleteProfileForm, RegistrationForm, LoginForm, TimelineForm, SettingsForm
+import json
 
 with app.app_context():
     db.create_all()
